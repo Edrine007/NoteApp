3a854718a6e2c4244b7b599d984cae9734c3c889
Edrine Matrix <edrine.matrix@gmail.com>
2022-09-26 01:36:41 -0400

    deleted css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :notes
   get 'home/index'
-  #get 'index' => 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "notes#index"
 end
