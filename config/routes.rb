9174310a44626cb3397104ba0d1d174b4b216a9d
Edrine Matrix <edrine.matrix@gmail.com>
2022-09-26 01:31:42 -0400

    added

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  #root "notes#index"
+  root "notes#index"
 end
